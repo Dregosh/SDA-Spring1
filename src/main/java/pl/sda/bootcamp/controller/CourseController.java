bfef4ed2434d39bb0dc66c03d37e1428dd462841
Dregosh <matello@upcpoczta.pl>
2020-10-29 20:13:32 +0100

    Feature: add first draft of Administration Panel

--- src/main/java/pl/sda/bootcamp/controller/CourseController.java
+++ src/main/java/pl/sda/bootcamp/controller/CourseController.java
@@ -7,6 +7,8 @@ import pl.sda.bootcamp.model.Course;
 import pl.sda.bootcamp.model.Student;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,49 +16,14 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "/kurs")
 public class CourseController {
     private List<String> cities;
+    private List<String> teachers;
     private List<Course> courses;
     private Student newStudent;
 
     public CourseController() {
-        this.courses = List.of(
-                Course.builder().id(1)
-                      .name("Java Developer")
-                      .city("Warszawa")
-                      .beginDate(LocalDate.of(2020, 11, 14))
-                      .endDate(LocalDate.of(2021, 7, 18))
-                      .mode("weekendowy")
-                      .price(7999)
-                      .build(),
-                Course.builder().id(2)
-                      .name("C++ od podstaw")
-                      .city("Kraków")
-                      .beginDate(LocalDate.of(2020, 12, 7))
-                      .endDate(LocalDate.of(2021, 8, 18))
-                      .mode("weekendowy")
-                      .price(6999)
-                      .build(),
-                Course.builder().id(3)
-                      .name("Angular Developer")
-                      .city("Szczecin")
-                      .beginDate(LocalDate.of(2020, 12, 14))
-                      .endDate(LocalDate.of(2021, 3, 15))
-                      .mode("dzienny")
-                      .price(4999)
-                      .build(),
-                Course.builder().id(4)
-                      .name("Tester manualny")
-                      .city("Warszawa")
-                      .beginDate(LocalDate.of(2021, 1, 14))
-                      .endDate(LocalDate.of(2021, 5, 25))
-                      .mode("dzienny")
-                      .price(4499)
-                      .build()
-        );
-        this.cities = this.courses.stream()
-                                  .map(Course::getCity)
-                                  .distinct()
-                                  .sorted()
-                                  .collect(Collectors.toList());
+        initializeCitiesList();
+        initializeTeachersList();
+        initializeCoursesList();
         this.newStudent = null;
     }
 
@@ -66,20 +33,11 @@ public class CourseController {
         return "course/list";
     }
 
-    @GetMapping("/dodaj")
-    public String add(Model model) {
-        model.addAttribute("cities", cities);
-        model.addAttribute("course", Course.builder().build());
-        return "course/add";
-    }
-
     @PostMapping("/dodaj")
-    public String create(@ModelAttribute Course course,
-                         Model model) {
-        model.addAttribute("cities", cities);
-        model.addAttribute("course", Course.builder().build());
-        model.addAttribute("createdCourse", course);
-        return "course/add";
+    public String create(@ModelAttribute Course course) {
+        course.setId(this.courses.size() + 1);
+        this.courses.add(course);
+        return "redirect:/kurs/adminpanel";
     }
 
     @GetMapping("/zapis/{courseId}")
@@ -100,6 +58,72 @@ public class CourseController {
         this.newStudent.setPhone(newStudent.getPhone());
         System.out.println(this.newStudent);
         return "redirect:/kurs/lista";
+    }
+
+    @GetMapping("/adminpanel")
+    public String adminPanel(Model model) {
+        model.addAttribute("courses", courses);
+        model.addAttribute("cities", cities);
+        model.addAttribute("teachers", teachers);
+        model.addAttribute("newCourse", Course.builder().build());
+        return "course/adminpanel";
+    }
+
+    private void initializeCitiesList() {
+        this.cities = new ArrayList<>();
+        this.cities.add("Warszawa");
+        this.cities.add("Gdańsk");
+        this.cities.add("Wrocław");
+        this.cities.add("Szczecin");
+        this.cities.sort(Comparator.naturalOrder());
+    }
+
+    private void initializeTeachersList() {
+        this.teachers = new ArrayList<>();
+        this.teachers.add("Nowak Jan");
+        this.teachers.add("Kowalski Adam");
+        this.teachers.add("Kłos Kamil");
+        this.teachers.sort(Comparator.naturalOrder());
+    }
+
+    private void initializeCoursesList() {
+        this.courses = new ArrayList<>();
+        this.courses.add(Course.builder().id(1)
+                               .name("Java Developer")
+                               .city(this.cities.get(0))
+                               .beginDate(LocalDate.of(2020, 11, 14))
+                               .endDate(LocalDate.of(2021, 7, 18))
+                               .mode("weekendowy")
+                               .price(7999)
+                               .teacher(this.teachers.get(0))
+                               .build());
+        this.courses.add(Course.builder().id(2)
+                               .name("C++ od podstaw")
+                               .city(this.cities.get(1))
+                               .beginDate(LocalDate.of(2020, 12, 7))
+                               .endDate(LocalDate.of(2021, 8, 18))
+                               .mode("weekendowy")
+                               .price(6999)
+                               .teacher(this.teachers.get(1))
+                               .build());
+        this.courses.add(Course.builder().id(3)
+                               .name("Angular Developer")
+                               .city(this.cities.get(2))
+                               .beginDate(LocalDate.of(2020, 12, 14))
+                               .endDate(LocalDate.of(2021, 3, 15))
+                               .mode("dzienny")
+                               .price(4999)
+                               .teacher(this.teachers.get(0))
+                               .build());
+        this.courses.add(Course.builder().id(4)
+                               .name("Tester manualny")
+                               .city(this.cities.get(0))
+                               .beginDate(LocalDate.of(2021, 1, 14))
+                               .endDate(LocalDate.of(2021, 5, 25))
+                               .mode("dzienny")
+                               .price(4499)
+                               .teacher(this.teachers.get(2))
+                               .build());
     }
 
     /*@GetMapping("/lista")
--- src/main/java/pl/sda/bootcamp/model/Course.java
+++ src/main/java/pl/sda/bootcamp/model/Course.java
@@ -3,6 +3,7 @@ package pl.sda.bootcamp.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -13,8 +14,14 @@ public class Course {
     private Integer id;
     private String name;
     private String city;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate beginDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+
     private String mode;
     private Integer price;
+    private String teacher;
 }
